23eeb11c46098fb243c6ffd336e19736f9f829d3
ktakahashi24 <k.taka4114@gmail.com>
2020-11-03 15:59:30 +0900

    add env to client

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3010',
+    origin: process.env.CLIENT_HOST,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
   await app.listen(3000);
