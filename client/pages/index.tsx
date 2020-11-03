d6ab0c1186274e3f001d9328f241dd1828084350
ktakahashi24 <k.taka4114@gmail.com>
2020-11-03 15:51:44 +0900

    connect client to servet

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,36 @@
-function HomePage() {
-  return <div>Welcome to Next.js Typesctipt!</div>
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const HomePage = () => {
+  const [test, setTest] = useState('test');
+  useEffect(() => {
+    setTest('initialize');
+  
+    const axiosBase = require('axios');
+    const axios = axiosBase.create({
+      baseURL: 'http://localhost:3011', // バックエンドB のURL:port を指定する
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+      },
+      responseType: 'json'  
+    });
+
+    axios.get('/', {})
+    .then(function(response) {
+
+      // 返ってきたレスポンスはそのまま加工せずに callback で呼び出し元へ渡す
+      console.log(response)
+    })
+    .catch(function(error) {
+      console.log('ERROR!! occurred in Backend.')
+    });
+
+  });
+
+  return (
+  <div>Welcome to Next.js Typesctipt!!!!{test}</div>
+  )
 }
 
 export default HomePage
