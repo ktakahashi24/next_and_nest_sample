692156477f08cbf12c89f10e576c881aa223e255
ktakahashi24 <k.taka4114@gmail.com>
2020-11-22 08:37:06 +0900

    add dotenv settings to next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,28 +1,18 @@
-const {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} = require('next/constants')
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
-// This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
-module.exports = (phase) => {
-  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  // when `next build` or `npm run build` is used
-  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
-  // when `next build` or `npm run build` is used
-  const isStaging =
-    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
-
-  const env = {
-    SERVER_HOST: (() => {
-      if (isDev) return 'http://localhost:3011'
-      if (isProd) return 'prod'
-      if (isStaging) return 'staging'
-    })(),
+module.exports = {
+  target: "serverless",
+  webpack: config => {
+    config.plugins = config.plugins || []
+    config.plugins = [
+      ...config.plugins,
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
+    return config
   }
-
-  // next.config.js object
-  return {
-    env,
-  }
-}
+};
