7c02ed04a1ccfd1fb292ef8c9fa9d41aadd81147
ktakahashi24 <k.taka4114@gmail.com>
2020-11-21 12:02:57 +0900

    add reset css

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,19 +2,17 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import Header from '../layouts/header';
+import '../styles/reset.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
+      <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
       {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
       {/* <link rel="manifest" href="/manifest.json" /> */}
     </Head>
     <Header />
-    <div css='width: 90%; margin: 0 auto;'>
+    <div css='width: 100vw; height: 100vh'>
       <Component {...pageProps} />
     </div>
   </>
